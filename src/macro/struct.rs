FlorianZ/tpm2-library

commit a5eb16a0cd458363b0e882e50019eb61c24227e7

refactor!(tpm2_protocol): drop tpm_response! and TpmParameters
Used the pattern matching mechanism of `macro_rules!`, turning pre-existing
parameters as self-documenting. In addition, a named parameter called
`kind` was added for the sake of  differentiating commands and responses.

The refined macro was also extended to be able to populate the parameter
area.

This was applied to the pre-existing commands and responses as follows:

1. Substituted each `tpm_struct!` with updated declaration.
2. Substituted each `tpm_response!` with a new `tpm_struct!` declaration.
   This removes ambiguity between responses, some of which are using
   `tpm_struct!`, and some using `tpm_response!` depending on whether they
   have a parameter area, or not.

Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>

src/macro/struct.rs
@@ -6,20 +6,61 @@
 macro_rules! tpm_struct {
     (
         $(#[$meta:meta])*
-        $name:ident,
-        $cc:expr,
-        $no_sessions:expr,
-        $with_sessions:expr,
-        $handles:expr,
-        {
-            $(pub $field_name:ident: $field_type:ty),*
+        kind: Command,
+        name: $name:ident,
+        cc: $cc:expr,
+        no_sessions: $no_sessions:expr,
+        with_sessions: $with_sessions:expr,
+        handles: {
+            $(pub $handle_field:ident: $handle_type:ty),*
+            $(,)?
+        },
+        parameters: {
+            $(pub $param_field:ident: $param_type:ty),*
             $(,)?
         }
     ) => {
-        $crate::tpm_struct! {
-            $(#[$meta])*
-            pub struct $name {
-                $(pub $field_name: $field_type,)*
+        $(#[$meta])*
+        pub struct $name {
+            $(pub $handle_field: $handle_type,)*
+            $(pub $param_field: $param_type,)*
+        }
+
+        impl $crate::TpmSized for $name {
+            const SIZE: usize = 0 $(+ <$handle_type>::SIZE)* $(+ <$param_type>::SIZE)*;
+            fn len(&self) -> usize {
+                0 $(+ $crate::TpmSized::len(&self.$handle_field))* $(+ $crate::TpmSized::len(&self.$param_field))*
+            }
+        }
+
+        impl $crate::TpmBuild for $name {
+            #[allow(unused_variables)]
+            fn build(&self, writer: &mut $crate::TpmWriter) -> $crate::TpmResult<()> {
+                $($crate::TpmBuild::build(&self.$handle_field, writer)?;)*
+                $($crate::TpmBuild::build(&self.$param_field, writer)?;)*
+                Ok(())
+            }
+        }
+
+        impl $crate::TpmParse for $name {
+            #[allow(unused_mut)]
+            fn parse(buf: &[u8]) -> $crate::TpmResult<(Self, &[u8])> {
+                let mut cursor = buf;
+                $(
+                    let ($handle_field, tail) = <$handle_type>::parse(cursor)?;
+                    cursor = tail;
+                )*
+                $(
+                    let ($param_field, tail) = <$param_type>::parse(cursor)?;
+                    cursor = tail;
+                )*
+                Ok((
+                    Self {
+                        $($handle_field,)*
+                        $($param_field,)*
+                    },
+                    cursor,
+                ))
             }
         }
 
@@ -27,7 +68,105 @@ macro_rules! tpm_struct {
             const COMMAND: $crate::data::TpmCc = $cc;
             const NO_SESSIONS: bool = $no_sessions;
             const WITH_SESSIONS: bool = $with_sessions;
-            const HANDLES: usize = $handles;
+            const HANDLES: usize = 0 $(+ {let _ = stringify!($handle_field); 1})*;
+        }
+    };
+
+    (
+        $(#[$meta:meta])*
+        kind: Response,
+        name: $name:ident,
+        cc: $cc:expr,
+        no_sessions: $no_sessions:expr,
+        with_sessions: $with_sessions:expr,
+        handles: {
+            $(pub $handle_field:ident: $handle_type:ty),*
+            $(,)?
+        },
+        parameters: {
+            $(pub $param_field:ident: $param_type:ty),*
+            $(,)?
+        }
+    ) => {
+        $(#[$meta])*
+        pub struct $name {
+            $(pub $handle_field: $handle_type,)*
+            $(pub $param_field: $param_type,)*
+        }
+
+        impl $crate::TpmSized for $name {
+            const SIZE: usize = 0 $(+ <$handle_type>::SIZE)* $(+ <$param_type>::SIZE)*;
+            fn len(&self) -> usize {
+                let params_len: usize = 0 $(+ $crate::TpmSized::len(&self.$param_field))*;
+                let handles_len: usize = 0 $(+ $crate::TpmSized::len(&self.$handle_field))*;
+                let parameter_area_size_field_len: usize = if params_len > 0 || $with_sessions {
+                    core::mem::size_of::<u32>()
+                } else {
+                    0
+                };
+                handles_len + parameter_area_size_field_len + params_len
+            }
+        }
+
+        impl $crate::TpmBuild for $name {
+            fn build(&self, writer: &mut $crate::TpmWriter) -> $crate::TpmResult<()> {
+                let params_len: usize = 0 $(+ $crate::TpmSized::len(&self.$param_field))*;
+                $($crate::TpmBuild::build(&self.$handle_field, writer)?;)*
+                if params_len > 0 || $with_sessions {
+                    let params_len_u32 = u32::try_from(params_len)
+                        .map_err(|_| $crate::TpmErrorKind::ValueTooLarge)?;
+                    $crate::TpmBuild::build(&params_len_u32, writer)?;
+                }
+                $($crate::TpmBuild::build(&self.$param_field, writer)?;)*
+                Ok(())
+            }
+        }
+
+        impl $crate::TpmParse for $name {
+            #[allow(unused_mut)]
+            fn parse(buf: &[u8]) -> $crate::TpmResult<(Self, &[u8])> {
+                let mut cursor = buf;
+                $(
+                    let ($handle_field, tail) = <$handle_type>::parse(cursor)?;
+                    cursor = tail;
+                )*
+
+                let (mut params_cursor, final_tail) = if !cursor.is_empty() && ($with_sessions || $no_sessions) {
+                    let (size, buf) = u32::parse(cursor)?;
+                    let size = size as usize;
+                    if buf.len() < size {
+                        return Err($crate::TpmErrorKind::Boundary);
+                    }
+                    let (param_data, final_tail) = buf.split_at(size);
+                    (param_data, final_tail)
+                } else {
+                    (cursor, &[][..])
+                };
+
+                $(
+                    let ($param_field, tail) = <$param_type>::parse(params_cursor)?;
+                    params_cursor = tail;
+                )*
+
+                if !params_cursor.is_empty() {
+                    return Err($crate::TpmErrorKind::TrailingData);
+                }
+
+                Ok((
+                    Self {
+                        $($handle_field,)*
+                        $($param_field,)*
+                    },
+                    final_tail,
+                ))
+            }
+        }
+
+        impl $crate::message::TpmHeader for $name {
+            const COMMAND: $crate::data::TpmCc = $cc;
+            const NO_SESSIONS: bool = $no_sessions;
+            const WITH_SESSIONS: bool = $with_sessions;
+            const HANDLES: usize = 0 $(+ {let _ = stringify!($handle_field); 1})*;
         }
     };
 

src/lib.rs
@@ -29,19 +29,16 @@ pub mod buffer;
 pub mod data;
 pub mod list;
 pub mod message;
-pub mod parameters;
 
 use crate::data::TpmAlgId;
+pub use buffer::TpmBuffer;
 use core::{
     convert::{From, TryFrom},
     fmt,
     mem::size_of,
     result::Result,
 };
-
-pub use buffer::TpmBuffer;
 pub use list::TpmList;
-pub use parameters::TpmParameters;
 
 tpm_handle! {
     #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]

src/macro/mod.rs
@@ -4,7 +4,6 @@
 
 pub mod r#enum;
 pub mod integer;
-pub mod response;
 pub mod r#struct;
 
 #[macro_export]

src/macro/response.rs
@@ -1,84 +1,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-// Copyright (c) 2025 Opinsys Oy
-// Copyright (c) 2024-2025 Jarkko Sakkinen
-
-#[macro_export]
-macro_rules! tpm_response {
-    (
-        $(#[$meta:meta])*
-        $name:ident,
-        $cc:expr,
-        $no_sessions:expr,
-        $with_sessions:expr,
-        $(pub $handle_field:ident: $handle_type:ty,)*
-        {
-            $(pub $param_field:ident: $param_type:ty),*
-            $(,)?
-        }
-    ) => {
-        $(#[$meta])*
-        pub struct $name {
-            $(pub $handle_field: $handle_type,)*
-            $(pub $param_field: $param_type,)*
-        }
-
-        impl $crate::message::TpmHeader for $name {
-            const COMMAND: $crate::data::TpmCc = $cc;
-            const NO_SESSIONS: bool = $no_sessions;
-            const WITH_SESSIONS: bool = $with_sessions;
-            const HANDLES: usize = 0 $(+ {let _ = stringify!($handle_field); 1})*;
-        }
-
-        impl $crate::TpmSized for $name {
-            const SIZE: usize = 0 $(+ <$handle_type>::SIZE)* $(+ <$param_type>::SIZE)*;
-            fn len(&self) -> usize {
-                let params_len: usize = 0 $(+ $crate::TpmSized::len(&self.$param_field))*;
-                let handles_len: usize = 0 $(+ $crate::TpmSized::len(&self.$handle_field))*;
-                let parameter_area_size_field_len: usize = core::mem::size_of::<u32>();
-                handles_len + parameter_area_size_field_len + params_len
-            }
-        }
-
-        impl $crate::TpmBuild for $name {
-            fn build(&self, writer: &mut $crate::TpmWriter) -> $crate::TpmResult<()> {
-                let params_len: usize = 0 $(+ $crate::TpmSized::len(&self.$param_field))*;
-                let params_len_u32 = u32::try_from(params_len)
-                    .map_err(|_| $crate::TpmErrorKind::ValueTooLarge)?;
-
-                $($crate::TpmBuild::build(&self.$handle_field, writer)?;)*
-                $crate::TpmBuild::build(&params_len_u32, writer)?;
-                $($crate::TpmBuild::build(&self.$param_field, writer)?;)*
-
-                Ok(())
-            }
-        }
-
-        impl $crate::TpmParse for $name {
-            fn parse(buf: &[u8]) -> $crate::TpmResult<(Self, &[u8])> {
-                #[allow(unused_mut)]
-                let mut cursor = buf;
-                $(
-                    let ($handle_field, tail) = <$handle_type>::parse(cursor)?;
-                    cursor = tail;
-                )*
-
-                #[allow(unused_mut)]
-                let (mut params, tail) = $crate::TpmParameters::new(cursor)?;
-                $(
-                    let $param_field = params.parse::<$param_type>()?;
-                )*
-                if !params.is_empty() {
-                    return Err($crate::TpmErrorKind::TrailingData);
-                }
-
-                Ok((
-                    Self {
-                        $( $handle_field, )*
-                        $( $param_field, )*
-                    },
-                    tail,
-                ))
-            }
-        }
-    };
-}

commit f666d267b79bd5f1509b18500fa65a479a7df80b

fix(tpm2_protocol): correct serialization logic
The previous refactoring to unify command and response generation was
incomplete, causing an impedance mismatch between the in-memory struct
representations and the on-the-wire format. This resulted in several test
failures related to incorrect buffer parsing and construction.

Complete the refactoring by making the builder and parser act as adapters
for this mismatch:

1. Command building and parsing has been fixed. The logic now correctly
   handles the session area, which is located between the handle and
   parameter areas on the wire. `tpm_build_command` now assembles these
   parts in the correct order, and `tpm_parse_command` correctly
   disassembles them and reconstructs a contiguous buffer for the
   struct-level parsers.

2. Response serialization in the `tpm_struct!` macro is corrected.  The
   `parameterSize` field is now only included if the corresponding command
   can have sessions (`with_sessions: true`), which aligns with the TCG
   specification.

These changes resolve all outstanding test failures related to the
refactoring. Additionally, a test case was cleaned up to remove magic
numbers, improving its readability.

Fixes: a5eb16a0cd45 ("refactor!(tpm2_protocol): drop tpm_response! and TpmParameters")
Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>

src/macro/struct.rs
@@ -99,7 +99,7 @@ macro_rules! tpm_struct {
             fn len(&self) -> usize {
                 let params_len: usize = 0 $(+ $crate::TpmSized::len(&self.$param_field))*;
                 let handles_len: usize = 0 $(+ $crate::TpmSized::len(&self.$handle_field))*;
-                let parameter_area_size_field_len: usize = if params_len > 0 || $with_sessions {
+                let parameter_area_size_field_len: usize = if $with_sessions {
                     core::mem::size_of::<u32>()
                 } else {
                     0
@@ -112,7 +112,7 @@ macro_rules! tpm_struct {
             fn build(&self, writer: &mut $crate::TpmWriter) -> $crate::TpmResult<()> {
                 let params_len: usize = 0 $(+ $crate::TpmSized::len(&self.$param_field))*;
                 $($crate::TpmBuild::build(&self.$handle_field, writer)?;)*
-                if params_len > 0 || $with_sessions {
+                if $with_sessions {
                     let params_len_u32 = u32::try_from(params_len)
                         .map_err(|_| $crate::TpmErrorKind::ValueTooLarge)?;
                     $crate::TpmBuild::build(&params_len_u32, writer)?;
@@ -131,7 +131,7 @@ macro_rules! tpm_struct {
                     cursor = tail;
                 )*
 
-                let (mut params_cursor, final_tail) = if !cursor.is_empty() && ($with_sessions || $no_sessions) {
+                let (mut params_cursor, final_tail) = if $with_sessions {
                     let (size, buf) = u32::parse(cursor)?;
                     let size = size as usize;
                     if buf.len() < size {

src/message/build.rs
@@ -5,7 +5,7 @@
 use crate::{
     data::{TpmRc, TpmSt, TpmsAuthCommand, TpmsAuthResponse},
     message::{TpmHeader, TPM_HEADER_SIZE},
-    TpmBuild, TpmErrorKind, TpmResult, TpmSized,
+    TpmBuild, TpmErrorKind, TpmResult, TpmSized, TpmWriter, TPM_MAX_COMMAND_SIZE,
 };
 use core::mem::size_of;
 
@@ -17,7 +17,6 @@ use core::mem::size_of;
 pub fn tpm_build_command<C>(
     command: &C,
     tag: TpmSt,
-    handles: Option<&[u32]>,
     sessions: &[TpmsAuthCommand],
     writer: &mut crate::TpmWriter,
 ) -> TpmResult<()>
@@ -48,13 +47,19 @@ where
         }
     }
 
-    let handles = handles.unwrap_or(&[]);
-    if handles.len() != C::HANDLES {
+    let mut temp_body_buf = [0u8; TPM_MAX_COMMAND_SIZE];
+    let body_len = {
+        let mut temp_writer = TpmWriter::new(&mut temp_body_buf);
+        command.build(&mut temp_writer)?;
+        temp_writer.len()
+    };
+    let body_bytes = &temp_body_buf[..body_len];
+
+    let handle_area_size = C::HANDLES * size_of::<u32>();
+    if body_len < handle_area_size {
         return Err(TpmErrorKind::Unreachable);
     }
-
-    let handle_area_len = core::mem::size_of_val(handles);
-    let parameters_len = command.len();
+    let (handle_bytes, param_bytes) = body_bytes.split_at(handle_area_size);
 
     let auth_area_len = if tag == TpmSt::Sessions {
         let sessions_len: usize = sessions.iter().map(TpmSized::len).sum();
@@ -63,7 +68,7 @@ where
         0
     };
 
-    let total_body_len = handle_area_len + auth_area_len + parameters_len;
+    let total_body_len = handle_bytes.len() + auth_area_len + param_bytes.len();
     let command_size =
         u32::try_from(TPM_HEADER_SIZE + total_body_len).map_err(|_| TpmErrorKind::ValueTooLarge)?;
 
@@ -71,9 +76,7 @@ where
     command_size.build(writer)?;
     (C::COMMAND as u32).build(writer)?;
 
-    for handle in handles {
-        handle.build(writer)?;
-    }
+    writer.write_bytes(handle_bytes)?;
 
     if tag == TpmSt::Sessions {
         let sessions_len_u32 = u32::try_from(auth_area_len - size_of::<u32>())
@@ -84,7 +87,7 @@ where
         }
     }
 
-    command.build(writer)
+    writer.write_bytes(param_bytes)
 }
 
 /// Builds a TPM response.

src/message/parse.rs
@@ -8,9 +8,9 @@ use super::{
 };
 use crate::{
     data::{TpmCc, TpmRc, TpmSt, TpmsAuthCommand, TpmsAuthResponse},
-    TpmErrorKind, TpmNotDiscriminant, TpmParse, TpmResult,
+    TpmErrorKind, TpmNotDiscriminant, TpmParse, TpmResult, TPM_MAX_COMMAND_SIZE,
 };
-use core::convert::TryFrom;
+use core::{convert::TryFrom, mem::size_of};
 
 /// The result of parsing a TPM response, containing either the successfully parsed
 /// body and auth areas (with a success or warning code) or a fatal error code.
@@ -34,7 +34,7 @@ pub fn tpm_parse_command(buf: &[u8]) -> TpmResult<(TpmHandles, TpmCommandBody, T
         TpmErrorKind::NotDiscriminant("TpmSt", TpmNotDiscriminant::Unsigned(u64::from(tag_raw)))
     })?;
     let (size, buf) = u32::parse(buf)?;
-    let (cc_raw, mut buf) = u32::parse(buf)?;
+    let (cc_raw, body_buf) = u32::parse(buf)?;
 
     if command_len != size as usize {
         return Err(TpmErrorKind::Boundary);
@@ -65,23 +65,20 @@ pub fn tpm_parse_command(buf: &[u8]) -> TpmResult<(TpmHandles, TpmCommandBody, T
         });
     }
 
-    let mut handles = TpmHandles::new();
-    for _ in 0..dispatch.3 {
-        let (handle, rest) = u32::parse(buf)?;
-        handles
-            .try_push(handle)
-            .map_err(|_| TpmErrorKind::ValueTooLarge)?;
-        buf = rest;
+    let handle_area_size = dispatch.3 * size_of::<u32>();
+    if body_buf.len() < handle_area_size {
+        return Err(TpmErrorKind::Boundary);
     }
+    let (handle_area, after_handles) = body_buf.split_at(handle_area_size);
 
     let mut sessions = TpmAuthCommands::new();
-    let param_buf = if tag == TpmSt::Sessions {
-        let (auth_area_size, auth_buf) = u32::parse(buf)?;
+    let param_area = if tag == TpmSt::Sessions {
+        let (auth_area_size, buf_after_auth_size) = u32::parse(after_handles)?;
         let auth_area_size = auth_area_size as usize;
-        if auth_buf.len() < auth_area_size {
+        if buf_after_auth_size.len() < auth_area_size {
             return Err(TpmErrorKind::Boundary);
         }
-        let (mut auth_area, param_buf) = auth_buf.split_at(auth_area_size);
+        let (mut auth_area, param_area) = buf_after_auth_size.split_at(auth_area_size);
         while !auth_area.is_empty() {
             let (session, rest) = TpmsAuthCommand::parse(auth_area)?;
             sessions
@@ -92,15 +89,32 @@ pub fn tpm_parse_command(buf: &[u8]) -> TpmResult<(TpmHandles, TpmCommandBody, T
         if !auth_area.is_empty() {
             return Err(TpmErrorKind::TrailingData);
         }
-        param_buf
+        param_area
     } else {
-        buf
+        after_handles
     };
 
-    let (command_data, remainder) = (dispatch.4)(param_buf)?;
+    let mut temp_body_buf = [0u8; TPM_MAX_COMMAND_SIZE];
+    let full_body_len = handle_area.len() + param_area.len();
+    if full_body_len > temp_body_buf.len() {
+        return Err(TpmErrorKind::ValueTooLarge);
+    }
+    let full_body_for_parser = &mut temp_body_buf[..full_body_len];
+    full_body_for_parser[..handle_area.len()].copy_from_slice(handle_area);
+    full_body_for_parser[handle_area.len()..].copy_from_slice(param_area);
+
+    let (command_data, remainder) = (dispatch.4)(full_body_for_parser)?;
 
     if !remainder.is_empty() {
         return Err(TpmErrorKind::TrailingData);
+    }
+
+    let mut handles = TpmHandles::new();
+    let mut temp_handle_cursor = handle_area;
+    while !temp_handle_cursor.is_empty() {
+        let (handle, rest) = u32::parse(temp_handle_cursor)?;
+        handles.try_push(handle)?;
+        temp_handle_cursor = rest;
     }
 
     Ok((handles, command_data, sessions))

tests/runner.rs
@@ -5,7 +5,7 @@
 #![allow(clippy::all)]
 #![allow(clippy::pedantic)]
 
-use std::{convert::TryFrom, io::IsTerminal, string::ToString, vec::Vec};
+use std::{convert::TryFrom, io::IsTerminal, mem::size_of, string::ToString, vec::Vec};
 use tpm2_protocol::{
     build_tpm2b,
     data::{
@@ -19,7 +19,7 @@ use tpm2_protocol::{
         TpmFlushContextCommand, TpmFlushContextResponse, TpmGetCapabilityCommand, TpmHashCommand,
         TpmPcrEventResponse, TpmPcrReadCommand, TpmPcrReadResponse, TpmPolicyGetDigestResponse,
     },
-    TpmBuffer, TpmBuild, TpmErrorKind, TpmParse, TpmPersistent, TpmSession, TpmWriter,
+    TpmBuffer, TpmBuild, TpmErrorKind, TpmParse, TpmPersistent, TpmSession, TpmSized, TpmWriter,
     TPM_MAX_COMMAND_SIZE,
 };
 
@@ -35,7 +35,7 @@ fn hex_to_bytes(s: &str) -> Result<Vec<u8>, &'static str> {
 }
 
 fn bytes_to_hex(bytes: &[u8]) -> String {
-    bytes.iter().map(|b| format!("{:02x}", b)).collect()
+    bytes.iter().map(|b| format!("{b:02x}")).collect()
 }
 
 fn test_rc_base_from_raw_rc() {
@@ -138,7 +138,6 @@ fn test_build_get_capability_command() {
         tpm_build_command(
             &cmd,
             tpm2_protocol::data::TpmSt::NoSessions,
-            None,
             &[],
             &mut writer,
         )
@@ -163,7 +162,6 @@ fn test_build_hash_command() {
         tpm_build_command(
             &cmd,
             tpm2_protocol::data::TpmSt::NoSessions,
-            None,
             &[],
             &mut writer,
         )
@@ -204,10 +202,7 @@ fn test_build_pcr_read_response() {
         0xDE, 0xDE, 0xDE, 0xDE, 0xDE, 0xDE, 0xDE, 0xDE, 0xDE, 0xDE, 0xDE,
     ];
 
-    assert_eq!(
-        bytes_to_hex(&generated_bytes),
-        bytes_to_hex(&expected_bytes)
-    );
+    assert_eq!(bytes_to_hex(generated_bytes), bytes_to_hex(expected_bytes));
 }
 
 fn test_build_error_response() {
@@ -288,7 +283,6 @@ fn test_parse_get_capability_command() {
             tpm_build_command(
                 &cmd,
                 tpm2_protocol::data::TpmSt::NoSessions,
-                None,
                 &[],
                 &mut writer,
             )
@@ -326,7 +320,6 @@ fn test_parse_hash_command() {
             tpm_build_command(
                 &cmd,
                 tpm2_protocol::data::TpmSt::NoSessions,
-                None,
                 &[],
                 &mut writer,
             )
@@ -363,7 +356,6 @@ fn test_parse_flush_context_command() {
             tpm_build_command(
                 &cmd,
                 tpm2_protocol::data::TpmSt::NoSessions,
-                None,
                 &[],
                 &mut writer,
             )
@@ -408,7 +400,6 @@ fn test_parse_pcr_read_command() {
             tpm_build_command(
                 &cmd,
                 tpm2_protocol::data::TpmSt::NoSessions,
-                None,
                 &[],
                 &mut writer,
             )
@@ -436,7 +427,6 @@ fn test_parse_context_save_command() {
     let cmd = TpmContextSaveCommand {
         save_handle: 0x8000_0001.into(),
     };
-    let handles = [cmd.save_handle.0];
 
     let generated_bytes = {
         let mut buf = [0u8; TPM_MAX_COMMAND_SIZE];
@@ -445,7 +435,6 @@ fn test_parse_context_save_command() {
             tpm_build_command(
                 &cmd,
                 tpm2_protocol::data::TpmSt::NoSessions,
-                Some(&handles),
                 &[],
                 &mut writer,
             )
@@ -462,6 +451,7 @@ fn test_parse_context_save_command() {
 
     match tpm_parse_command(&generated_bytes) {
         Ok((res_handles, cmd_data, sessions)) => {
+            let handles = [cmd.save_handle.0];
             assert_eq!(res_handles.as_ref(), handles);
             assert_eq!(cmd_data, TpmCommandBody::ContextSave(cmd));
             if !sessions.is_empty() {
@@ -473,10 +463,9 @@ fn test_parse_context_save_command() {
 }
 
 fn test_parse_evict_control_command() {
-    let handles = [TpmRh::Owner as u32, 0x8000_0000];
     let cmd = TpmEvictControlCommand {
-        auth: handles[0].into(),
-        object_handle: handles[1].into(),
+        auth: (TpmRh::Owner as u32).into(),
+        object_handle: 0x8000_0000.into(),
         persistent_handle: TpmPersistent(0x8100_0001),
     };
     let mut sessions = TpmAuthCommands::new();
@@ -496,7 +485,6 @@ fn test_parse_evict_control_command() {
             tpm_build_command(
                 &cmd,
                 tpm2_protocol::data::TpmSt::Sessions,
-                Some(&handles),
                 &sessions,
                 &mut writer,
             )
@@ -508,6 +496,7 @@ fn test_parse_evict_control_command() {
 
     let (res_handles, res_cmd_data, res_sessions) = tpm_parse_command(&generated_bytes).unwrap();
 
+    let handles = [cmd.auth.0, cmd.object_handle.0];
     assert_eq!(res_handles.as_ref(), handles);
     assert_eq!(res_sessions, sessions);
     assert_eq!(res_cmd_data, TpmCommandBody::EvictControl(cmd));
@@ -515,12 +504,11 @@ fn test_parse_evict_control_command() {
 
 fn test_response_macro_parse_correctness() {
     let mut digests = tpm2_protocol::data::TpmlDigestValues::new();
-    digests
-        .try_push(tpm2_protocol::data::TpmtHa {
-            hash_alg: TpmAlgId::Sha256,
-            digest: tpm2_protocol::data::TpmuHa::Sha256([0xA1; 32]),
-        })
-        .unwrap();
+    let digest = tpm2_protocol::data::TpmtHa {
+        hash_alg: TpmAlgId::Sha256,
+        digest: tpm2_protocol::data::TpmuHa::Sha256([0xA1; 32]),
+    };
+    digests.try_push(digest).unwrap();
     let original_resp = TpmPcrEventResponse { digests };
 
     let mut body_buf = [0u8; 1024];
@@ -531,12 +519,17 @@ fn test_response_macro_parse_correctness() {
     };
     let response_body_bytes = &body_buf[..body_len];
 
-    assert_eq!(body_len, 42);
-    assert_eq!(&response_body_bytes[0..4], &38u32.to_be_bytes());
+    let expected_len = size_of::<u32>() + original_resp.digests.len();
+    assert_eq!(body_len, expected_len);
+    assert_eq!(
+        &response_body_bytes[0..4],
+        &u32::to_be_bytes(original_resp.digests.len() as u32)
+    );
+    assert_eq!(&response_body_bytes[4..8], &1u32.to_be_bytes());
 
     let result = TpmPcrEventResponse::parse(response_body_bytes);
 
-    assert!(result.is_ok(), "command parsing failed: {:?}", result.err());
+    assert!(result.is_ok(), "command parsing failed: {result:?}");
     let (parsed_resp, tail) = result.unwrap();
     assert_eq!(parsed_resp, original_resp, "response mismatch");
     assert!(tail.is_empty(), "tail data");
@@ -622,8 +615,7 @@ fn test_parse_policy_get_digest_response() {
 
     assert!(
         result.is_ok(),
-        "Parsing should succeed with the fixed parser. Result: {:?}",
-        result
+        "Parsing should succeed with the fixed parser. Result: {result:?}"
     );
     let (parsed_resp, remainder) = result.unwrap();
     assert_eq!(