326c2ab2bf6e31555d105861f7972021fae8efb9
Jarkko Sakkinen <jarkko@kernel.org>
2025-11-05 03:12:31 +0200

    chore: bump version to 0.3.1
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- tests/policy.rs
+++ tests/policy.rs
@@ -1,19 +1,17 @@
 //! SPDX-License-Identifier: MIT OR Apache-2.0
 //! Copyright (c) 2024-2025 Jarkko Sakkinen
 
-use std::str;
 use tpm2_policy_language::PolicyState;
 use tpm2_tpmkey::TpmKey;
 
-const PEM_ORIGINAL_BYTES: &[u8] = include_bin!("policy_1.key");
+const PEM_ORIGINAL_STR: &str = include_str!("policy_1.key");
 
 #[test]
 fn test_complex_policy_roundtrip() {
-    let context = PolicyState::new();
-    let pem_original = str::from_utf8(PEM_ORIGINAL_BYTES).expect("policy_1.key is not valid UTF-8");
+    let context = PolicyState::default();
 
-    let key_original =
-        TpmKey::from_pem(PEM_ORIGINAL_BYTES, &context).expect("Failed to parse original PEM");
+    let key_original = TpmKey::from_pem(PEM_ORIGINAL_STR.as_bytes(), &context)
+        .expect("Failed to parse original PEM");
 
     let pem_saved = key_original
         .to_pem(&context)
@@ -23,6 +21,5 @@ fn test_complex_policy_roundtrip() {
         .expect("Failed to parse re-serialized PEM");
 
     assert_eq!(key_original, key_reloaded);
-
-    assert_eq!(pem_original, pem_saved);
+    assert_eq!(PEM_ORIGINAL_STR, pem_saved);
 }
