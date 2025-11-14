b2a4d85d0e419e74c06647d4e4c5c88b52fe9b92
Jarkko Sakkinen <jarkko@kernel.org>
2025-11-14 16:40:22 +0200

    feat: TryFrom<Nid> for EccCurve
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- src/ecc.rs
+++ src/ecc.rs
@@ -109,6 +109,25 @@ impl From<EccCurve> for Nid {
     }
 }
 
+impl TryFrom<Nid> for EccCurve {
+    type Error = Error;
+
+    fn try_from(nid: Nid) -> Result<Self, Self::Error> {
+        match nid {
+            Nid::X9_62_PRIME192V1 => Ok(EccCurve::NistP192),
+            Nid::SECP224R1 => Ok(EccCurve::NistP224),
+            Nid::X9_62_PRIME256V1 => Ok(EccCurve::NistP256),
+            Nid::SECP384R1 => Ok(EccCurve::NistP384),
+            Nid::SECP521R1 => Ok(EccCurve::NistP521),
+            Nid::BRAINPOOL_P256R1 => Ok(EccCurve::BpP256R1),
+            Nid::BRAINPOOL_P384R1 => Ok(EccCurve::BpP384R1),
+            Nid::BRAINPOOL_P512R1 => Ok(EccCurve::BpP512R1),
+            Nid::SM2 => Ok(EccCurve::Sm2P256),
+            _ => Err(Error::InvalidEccCurve),
+        }
+    }
+}
+
 /// ECC public key parameters.
 #[derive(Debug, Clone)]
 pub struct EccPublicKey {
@@ -146,19 +165,7 @@ impl TryFrom<&PKey<Private>> for EccPublicKey {
         let ec_key = pkey.ec_key().map_err(|_| Error::InvalidEccParameters)?;
         let group = ec_key.group();
         let nid = group.curve_name().ok_or(Error::InvalidEccParameters)?;
-
-        let curve = match nid {
-            Nid::X9_62_PRIME192V1 => EccCurve::NistP192,
-            Nid::SECP224R1 => EccCurve::NistP224,
-            Nid::X9_62_PRIME256V1 => EccCurve::NistP256,
-            Nid::SECP384R1 => EccCurve::NistP384,
-            Nid::SECP521R1 => EccCurve::NistP521,
-            Nid::BRAINPOOL_P256R1 => EccCurve::BpP256R1,
-            Nid::BRAINPOOL_P384R1 => EccCurve::BpP384R1,
-            Nid::BRAINPOOL_P512R1 => EccCurve::BpP512R1,
-            Nid::SM2 => EccCurve::Sm2P256,
-            _ => return Err(Error::InvalidEccCurve),
-        };
+        let curve = EccCurve::try_from(nid)?;
 
         let mut ctx = BigNumContext::new().map_err(|_| Error::OutOfMemory)?;
         let (x, y) = crate::make_tpm_point(ec_key.public_key(), group, &mut ctx)?;
