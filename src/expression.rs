ec26e1c47f2675a3d1dac950ce7dcb6a00b2ffd3
Jarkko Sakkinen <jarkko@kernel.org>
2025-11-14 17:07:20 +0200

    docs: fix copyright and SPDX platters
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- src/expression.rs
+++ src/expression.rs
@@ -1,6 +1,6 @@
-//! SPDX-License-Identifier: MIT OR Apache-2.0
-//! Copyright (c) 2025 Opinsys Oy
-//! Copyright (c) 2024-2025 Jarkko Sakkinen
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// Copyright (c) 2025 Opinsys Oy
+// Copyright (c) 2024-2025 Jarkko Sakkinen
 
 use crate::{
     build_and_branch, Auth, Error, Handle, HandleClass, HandleError, LanguageError,
