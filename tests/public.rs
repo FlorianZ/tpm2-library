058d160df2c996af5cd8fd0d2c0d98f3f970d304
Jarkko Sakkinen <jarkko@kernel.org>
2025-11-18 16:17:21 +0200

    Revert "remove: wrapper EccCurve type"
    This reverts commit a3a06e193ef6a8bdd867a4f92add336956537d12.

--- tests/public.rs
+++ tests/public.rs
@@ -8,10 +8,10 @@
 #![deny(clippy::pedantic)]
 
 use rstest::rstest;
-use tpm2_crypto::{TpmEccPublicKey, TpmPublicKey, TpmRsaPublicKey};
+use tpm2_crypto::{EccCurve, TpmEccPublicKey, TpmPublicKey, TpmRsaPublicKey};
 use tpm2_protocol::data::{
-    Tpm2bEccParameter, Tpm2bPublicKeyRsa, TpmAlgId, TpmEccCurve, TpmaObject, TpmtSymDefObject,
-    TpmuAsymScheme, TpmuPublicId, TpmuPublicParms,
+    Tpm2bEccParameter, Tpm2bPublicKeyRsa, TpmAlgId, TpmaObject, TpmtSymDefObject, TpmuAsymScheme,
+    TpmuPublicId, TpmuPublicParms,
 };
 
 const TEST_MODULUS: [u8; 256] = [1; 256];
@@ -59,11 +59,11 @@ fn test_rsa_to_public(#[case] hash_alg: TpmAlgId, #[case] key_bits: u16) {
 }
 
 #[rstest]
-#[case(TpmAlgId::Sha256, TpmEccCurve::NistP256, &TEST_COORD, &TEST_COORD)]
-#[case(TpmAlgId::Sha1, TpmEccCurve::NistP192, &[3; 24], &[4; 24])]
+#[case(TpmAlgId::Sha256, EccCurve::NistP256, &TEST_COORD, &TEST_COORD)]
+#[case(TpmAlgId::Sha1, EccCurve::NistP192, &[3; 24], &[4; 24])]
 fn test_ecc_to_public(
     #[case] hash_alg: TpmAlgId,
-    #[case] curve: TpmEccCurve,
+    #[case] curve: EccCurve,
     #[case] x_bytes: &[u8],
     #[case] y_bytes: &[u8],
 ) {
@@ -81,7 +81,7 @@ fn test_ecc_to_public(
     assert_eq!(public.auth_policy.len(), 0);
 
     if let TpmuPublicParms::Ecc(params) = public.parameters {
-        assert_eq!(params.curve_id, curve);
+        assert_eq!(params.curve_id, curve.into());
         assert_eq!(params.scheme.scheme, TpmAlgId::Ecdh);
         if let TpmuAsymScheme::Hash(details) = params.scheme.details {
             assert_eq!(details.hash_alg, hash_alg);
