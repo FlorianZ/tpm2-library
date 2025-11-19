0b5ef1fd717589d9f5ab2f6cf9d7559fda268da0
Jarkko Sakkinen <jarkko@kernel.org>
2025-11-19 02:28:20 +0200

    refactor: export oid constants
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- src/asn1.rs
+++ src/asn1.rs
@@ -10,11 +10,11 @@ use rasn::{
 };
 use tpm2_protocol::{constant::TPM_MAX_COMMAND_SIZE, TpmMarshal, TpmWriter};
 
-pub(crate) const OID_LOADABLE_KEY: ObjectIdentifier =
+pub const OID_LOADABLE_KEY: ObjectIdentifier =
     ObjectIdentifier::new_unchecked(std::borrow::Cow::Borrowed(&[2, 23, 133, 10, 1, 3]));
-pub(crate) const OID_IMPORTABLE_KEY: ObjectIdentifier =
+pub const OID_IMPORTABLE_KEY: ObjectIdentifier =
     ObjectIdentifier::new_unchecked(std::borrow::Cow::Borrowed(&[2, 23, 133, 10, 1, 4]));
-pub(crate) const OID_SEALED_DATA: ObjectIdentifier =
+pub const OID_SEALED_DATA: ObjectIdentifier =
     ObjectIdentifier::new_unchecked(std::borrow::Cow::Borrowed(&[2, 23, 133, 10, 1, 5]));
 
 pub(crate) fn tpm_marshal_array(objs: &[&dyn TpmMarshal]) -> Result<Vec<u8>, TpmKeyError> {
