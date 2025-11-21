67f7ba89fcdf2fbfd954cd0c8673ee910605ea6d
Jarkko Sakkinen <jarkko@kernel.org>
2025-11-21 18:15:13 +0200

    build: update release.sh
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
+# SPDX-License-Identifier: GPL-3-0-or-later
+# Copyright (c) 2024-2025 Jarkko Sakkinen
 # Copyright (c) 2025 Opinsys Oy
+
+set -e
 
 PREVIOUS="${1:-$PREV}"
 NEXT="${2:-$NEXT}"
@@ -9,8 +14,22 @@ if [ -z "$NEXT" ] || [ -z "$PREVIOUS" ]; then
   exit 1
 fi
 
+if git status --porcelain | grep .; then
+  echo "Working directory $PWD is not clean"
+  exit 1
+fi
+
+if grep "^version = $NEXT" Cargo.toml >/dev/null; then
+  echo "Version $NEXT is already set"
+fi
+
+sed -i "s/^version =.*/version = \"$NEXT\"/g" Cargo.toml
+cargo clippy --all-targets
+
+git commit -a -s -m "chore: bump version to $NEXT"
+
 MESSAGE=$(
-  echo "Release $NEXT"
+  echo "tpm-policy-language $NEXT"
   echo ""
   git log --pretty=format:"- %s" --no-merges "$PREVIOUS..HEAD"
 )
